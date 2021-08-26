97c18a4e91220cf6980ae1b0b90093a2b240565b
Einson <1519695805@qq.com>
2021-08-26 16:00:38 +0800

    add bst interface

--- tree/binarySearchTree.go
+++ tree/binarySearchTree.go
@@ -1,5 +1,7 @@
 package tree
 
+import "sync"
+
 type Node struct {
 	binaryTreeEntry
 	times  int
@@ -10,12 +12,64 @@ type Node struct {
 
 type BinarySearchTree struct {
 	root *Node
+	lock sync.Mutex
 }
 
-func (b *BinarySearchTree) Root() interface{} {
-	return b.root
+// IsNil 判断节点是否为nil
+func (t *BinarySearchTree) IsNil(n interface{}) bool {
+	return n == nil
 }
 
-func NewTreeRoot(node *Node) *BinarySearchTree {
-	return &BinarySearchTree{root: node}
+// Search 根据key查找节点
+func (t *BinarySearchTree) Search(key uint32) interface{} {
+	for cur := t.root; cur != nil; {
+		if cur.Key == key {
+			return cur
+		} else if key < cur.Key {
+			cur = cur.left
+		} else {
+			cur = cur.right
+		}
+	}
+	return nil
+}
+
+func (t *BinarySearchTree) Insert(i interface{}) interface{} {
+	panic("implement me")
+}
+
+func (t *BinarySearchTree) Delete(u uint32) interface{} {
+	panic("implement me")
+}
+
+func (t *BinarySearchTree) Predecessor(i interface{}, i2 interface{}) interface{} {
+	panic("implement me")
+}
+
+func (t *BinarySearchTree) Successor(i interface{}, i2 interface{}) interface{} {
+	panic("implement me")
+}
+
+func (t *BinarySearchTree) LeftRotate(i interface{}) interface{} {
+	panic("implement me")
+}
+
+func (t *BinarySearchTree) RightRotate(i interface{}) interface{} {
+	panic("implement me")
+}
+
+func (t *BinarySearchTree) Min(i interface{}) interface{} {
+	panic("implement me")
+}
+
+func (t *BinarySearchTree) Max(i interface{}) interface{} {
+	panic("implement me")
+}
+
+func (t *BinarySearchTree) Root() interface{} {
+	return t.root
+}
+
+func NewTreeRoot() *BinarySearchTree {
+	return new(BinarySearchTree)
 }
--- tree/binaryTree.go
+++ tree/binaryTree.go
@@ -5,5 +5,15 @@ type binaryTreeEntry struct {
 	Value interface{}
 }
 type IBinaryTree interface {
+	IsNil(interface{}) bool
 	Root() interface{}
+	Search(uint32) interface{}
+	Insert(interface{}) interface{}
+	Delete(uint32) interface{}
+	Predecessor(interface{}, interface{}) interface{}
+	Successor(interface{}, interface{}) interface{}
+	LeftRotate(interface{}) interface{}
+	RightRotate(interface{}) interface{}
+	Min(interface{}) interface{}
+	Max(interface{}) interface{}
 }
