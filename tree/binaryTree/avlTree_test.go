e422d3b168c623ef2c7c32d224f6cb8b3a9faac3
Einson <1519695805@qq.com>
2021-09-02 14:24:43 +0800

    add avl tree test interface

---
+++ tree/binaryTree/avlTree_test.go
@@ -0,0 +1,69 @@
+package binaryTree
+
+import (
+	"testing"
+)
+
+func TestAVLTree_Delete(t *testing.T) {
+
+}
+
+func TestAVLTree_InOrderTraverse(t *testing.T) {
+
+}
+
+func TestAVLTree_Insert(t *testing.T) {
+
+}
+
+func TestAVLTree_IsNil(t *testing.T) {
+
+}
+
+func TestAVLTree_LeftRightRotate(t *testing.T) {
+
+}
+
+func TestAVLTree_LeftRotate(t *testing.T) {
+
+}
+
+func TestAVLTree_Max(t *testing.T) {
+
+}
+
+func TestAVLTree_Min(t *testing.T) {
+
+}
+
+func TestAVLTree_PostOrderTraverse(t *testing.T) {
+
+}
+
+func TestAVLTree_PreOrderTraverse(t *testing.T) {
+
+}
+
+func TestAVLTree_Predecessor(t *testing.T) {
+
+}
+
+func TestAVLTree_RightLeftRotate(t *testing.T) {
+
+}
+
+func TestAVLTree_RightRotate(t *testing.T) {
+
+}
+
+func TestAVLTree_Root(t *testing.T) {
+
+}
+
+func TestAVLTree_Search(t *testing.T) {
+
+}
+
+func TestAVLTree_Successor(t *testing.T) {
+
+}
